2705170555999783ec594b574b0946bc7da3b990
mahmudulpro <mahmudulpro@gmail.com>
2018-05-31 00:17:23 +0600

    second commit

--- olsp/polls/views.py
+++ olsp/polls/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("<center><h1>Welcome To OLS</h1></center>")
+    
+
