837866fca67b2f1e483b9270313695fa3912a4d2
mahmudulpro <mahmudulpro@gmail.com>
2018-06-01 18:30:45 +0600

    first commit

--- olsp/api/urls.py
+++ olsp/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from django.conf.urls import url
+from api import views
 
 
 urlpatterns=[
-	path('', views.index, name='index'),	
+	url(r'^$', views.QuestionList.as_view()),
 ]
--- olsp/api/views.py
+++ olsp/api/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from api.models import Question
+from api.serializers import QuestionSerializer
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
-def index(request):
+class QuestionList(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'api/index.html'
 
-    return render(request, 'api/index.html')
+    def get(self, request):
+        queryset = Question.objects.all()
+        return Response({'questions': queryset})
